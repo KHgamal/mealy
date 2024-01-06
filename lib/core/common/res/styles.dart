6f1917bc1a3de21febe8957af329be011757154a
Rahma Gamal <rahmagm002@gmail.com>
2024-01-07 00:19:11 +0200

    adding colors and styles

---
+++ lib/core/common/res/styles.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mealy/core/common/res/colors.dart';
+
+class Styles {
+  static TextStyle textStyle12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: AllColors.black,
+  );
+  static TextStyle textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: AllColors.mainText,
+  );
+  static TextStyle textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AllColors.mainText,
+  );
+  static TextStyle textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AllColors.mainText,
+  );
+  static TextStyle textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AllColors.black,
+  );
+  static TextStyle textStyle32 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    color: AllColors.black,
+  );
+  static TextStyle textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: AllColors.white,
+  );
+  static TextStyle textStyle96 = TextStyle(
+    fontSize: 96,
+    fontWeight: FontWeight.w600,
+    color: AllColors.buttonMainColor,
+  );
+}
