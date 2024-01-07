747780e7840c9ab24026a3ed0a0cc388ae65f251
Dell <khokha122002kh@gmail.com>
2024-01-07 17:23:01 +0200

    add_font_family_and_change_app_name

--- lib/core/common/res/colors.dart
+++ lib/core/common/res/colors.dart
@@ -6,13 +6,13 @@ class AllColors {
   static Color buttonMainColor = const Color(0xFFF73042);
   static Color buttonBgColor = const Color(0xFFFEEAEC);
   static Color buttonTextColor = const Color(0xFFF9FBFC);
-  static Color disapledText = const Color(0xFF848484);
+  static Color disabledText = const Color(0xFF848484);
   static Color darkGray = const Color(0xFF384455);
   static Color gray = const Color(0xFFB3B3B3);
   static Color searchBar = const Color(0xFF333333);
   static Color calTextColor = const Color(0xFFF4F4F4);
   static Color subtitleColor = const Color(0xFF626262);
-  static Color unchoosenGender = const Color(0xFFF0F0F0);
+  static Color unChoosenGender = const Color(0xFFF0F0F0);
   static Color black = Colors.black;
   static Color white = Colors.white;
 }
