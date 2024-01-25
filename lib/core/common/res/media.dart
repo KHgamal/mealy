27e16fd69660de490128b10066183477897a38f8
Dell <khokha122002kh@gmail.com>
2024-01-25 08:52:49 +0200

    generate svg images

--- lib/core/common/res/media.dart
+++ lib/core/common/res/media.dart
@@ -1,14 +1,11 @@
 class Images {
-  static String splashIcon="assets/images/Svg red 1.svg";
-  static String splashBg="assets/images/Pattern.svg";
+  static String splashIcon="assets/images/splashIcon.svg";
+  static String splashBg="assets/images/splashBg.svg";
 
-  static String onBoarding1="assets/images/Rectangle 22.svg";
-  static String onBoarding2="assets/images/Rectangle 22 (1).svg";
-  static String onBoarding3="assets/images/Group 33303.svg";
-  static String titleIcon ="assets/images/food 9.svg";
-  static String googleIcon="assets/images/Group.svg";
-  static String facebookIcon="assets/images/Group (1).svg";
+  static String onBoarding1="assets/images/onBoarding1.svg";
+  static String onBoarding2="assets/images/onBoarding2.svg";
+  static String onBoarding3="assets/images/onBoarding3.svg";
 
-  static String logo="assets/images/Svg red 4.svg";
-  static String authBg="assets/images/Pattern (1).svg";
+  static String logo="assets/images/logo.svg";
+  static String authBg="assets/images/authBg.svg";
 }
