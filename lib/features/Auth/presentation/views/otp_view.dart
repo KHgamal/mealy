1200fe99c6182394dd77ec65f74bf34148dace8b
Dell <khokha122002kh@gmail.com>
2024-01-27 02:40:48 +0200

    complete OTP screen

--- lib/features/Auth/presentation/views/otp_view.dart
+++ lib/features/Auth/presentation/views/otp_view.dart
@@ -4,6 +4,7 @@ import 'package:mealy/features/Auth/presentation/widgets/otp.dart';
 import 'package:mealy/generated/l10n.dart';
 
 import '../../../../core/common/widgets/common_button.dart';
+import '../widgets/auth_header.dart';
 
 class OTPScreen extends StatelessWidget {
   const OTPScreen({super.key});
@@ -12,22 +13,33 @@ class OTPScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              Expanded(child: SizedBox()),
-              Otp(),
-              ResendCode(),
-              Expanded(
-                  flex: 2,
-                  child: SizedBox()),
-              CommonButton(txt:S.of(context).continue_text, onPressed:(){}, radius: 8,),
-              Expanded(
-                  flex: 3,
-                  child: SizedBox()),
-            ],
-          ),
+        body: Column(
+          children: [
+            AuthHeader(
+              title: S.of(context).authHeader3,
+              subTitle:  S.of(context).authSubTitle ,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+
+                    Expanded(child: SizedBox()),
+                    Otp(),
+                    ResendCode(),
+                    Expanded(
+                        flex: 2,
+                        child: SizedBox()),
+                    CommonButton(txt:S.of(context).continue_text, onPressed:(){}, radius: 8,),
+                    Expanded(
+                        flex: 3,
+                        child: SizedBox()),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
