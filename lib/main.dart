3ade410cc1681c6222154a7222ec18ebd4d7e824
rahmagamal217 <rahmagm002@gmail.com>
2024-01-28 19:38:27 +0200

    changing password view2

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:mealy/core/common/res/colors.dart';
 import 'package:mealy/features/Auth/presentation/views/login_view.dart';
+import 'package:mealy/features/Auth/presentation/widgets/changing_password_view2_body.dart';
 import 'package:mealy/generated/l10n.dart';
 
+import 'features/Auth/presentation/views/changing_password_view.dart';
+import 'features/Auth/presentation/views/changing_password_view2.dart';
 import 'features/Auth/presentation/views/create_account_view.dart';
 import 'features/Auth/presentation/views/otp_view.dart';
 import 'features/Auth/presentation/views/test.dart';
@@ -39,7 +42,9 @@ class MyApp extends StatelessWidget {
         EntranceView.id: (_) => const EntranceView(),
         LoginView.id: (_) => const LoginView(),
         OTPScreen.id: (_) => const OTPScreen(),
-        CreateAccountView.id: (_) => const  CreateAccountView()
+        CreateAccountView.id: (_) => const CreateAccountView(),
+        ChangingPasswordView.id: (_) => const ChangingPasswordView(),
+        ChangingPasswordView2.id: (_) => const ChangingPasswordView2()
       },
       home: const Test(),
     );
