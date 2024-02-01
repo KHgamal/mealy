122d34b933cba495b9533b64c0bf85cbeaa1100b
rahmagamal217 <rahmagm002@gmail.com>
2024-02-01 15:42:49 +0200

    fixes

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mealy/core/common/res/styles.dart';
 import 'package:mealy/generated/assets.dart';
 
