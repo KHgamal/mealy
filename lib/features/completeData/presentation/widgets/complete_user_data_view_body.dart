ddb2d21790abce7ba97168f46f6aadcfbb5cce49
Dell <khokha122002kh@gmail.com>
2024-02-07 20:31:24 +0200

    increase height between index circle and header (complete data views)

--- lib/features/completeData/presentation/widgets/complete_user_data_view_body.dart
+++ lib/features/completeData/presentation/widgets/complete_user_data_view_body.dart
@@ -20,9 +20,7 @@ class CompleteUserDataViewBody extends StatelessWidget {
           title: S.of(context).complete_Data,
           subTitle: S.of(context).authSubTitle,
         ),
-        const SizedBox(
-          height: 15,
-        ),
+        const SizedBox(height:25),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Column(
