5d3e2146b35b4f4b739b604f80c469e724ad31c6
Dell <khokha122002kh@gmail.com>
2024-03-15 17:53:53 +0200

    changes

--- lib/features/home/presentation/widgets/plan_container.dart
+++ lib/features/home/presentation/widgets/plan_container.dart
@@ -4,41 +4,45 @@ import 'package:mealy/core/common/res/styles.dart';
 
 import '../../../Subscriptions/presentation/widgets/options_element.dart';
 class PlanContainer extends StatelessWidget {
-  const PlanContainer({super.key, required this.title, required this.price, required this.days, required this.limitation});
+  const PlanContainer({super.key, required this.title, required this.price, required this.days, required this.limitation, required this.onTap});
   final String title,price,days,limitation;
+  final void Function() onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          side: BorderSide(width: 1, color: AllColors.unChoosenGender),
-          borderRadius: BorderRadius.circular(16),
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                  title,
-                  style:Styles.textStyleSemiBold18(context).copyWith(color: AllColors.mainText)
-              ),
-              Text(
-                price,
-                style:Styles.textStyleSemiBold18(context)
-              ),
-
-            ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(width: 1, color: AllColors.unChoosenGender),
+            borderRadius: BorderRadius.circular(16),
           ),
-          const SizedBox(height: 16),
-          OptionsElement(text: days,color: AllColors.mainText,),
-          const SizedBox(height: 8),
-          OptionsElement(text: limitation,color: AllColors.mainText,),
-        ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                    title,
+                    style:Styles.textStyleSemiBold18(context).copyWith(color: AllColors.mainText)
+                ),
+                Text(
+                  price,
+                  style:Styles.textStyleSemiBold18(context)
+                ),
+
+              ],
+            ),
+            const SizedBox(height: 16),
+            OptionsElement(text: days,color: AllColors.mainText,),
+            const SizedBox(height: 8),
+            OptionsElement(text: limitation,color: AllColors.mainText,),
+          ],
+        ),
       ),
     );
   }
