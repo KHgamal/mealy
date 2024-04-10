622cf6fb8d24e8d66ebe7608706d1946d6b4fe25
Dell <khokha122002kh@gmail.com>
2024-04-10 04:47:33 +0200

    connect date

--- lib/features/Meals/presentation/widgets/subscriptions_start_date_container.dart
+++ lib/features/Meals/presentation/widgets/subscriptions_start_date_container.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../core/common/res/colors.dart';
 import '../../../../core/common/res/styles.dart';
+import '../../../../generated/l10n.dart';
+import '../../../Subscriptions/presentation/controller/date controller/date_provider.dart';
+import '../../../profile/presentation/controller/app_language_provider/app_language_provider.dart';
 
 class SubscriptionsStartDateContainer extends StatelessWidget {
   const SubscriptionsStartDateContainer({
@@ -18,7 +23,8 @@ class SubscriptionsStartDateContainer extends StatelessWidget {
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9)),
       ),
       child: Text(
-          'يبدا الاشتراك يوم 5 ديسمبر',
+        '${S.of(context).SubscriptionStartsFrom} ${DateFormat.MMMMd( Provider.of<AppLanguage>(context).locale ==
+            const Locale('en')?"en_US":'ar_SA').format(Provider.of<DateProvider>(context). dateSelected)}',
           style:Styles.textStyleMedium12(context).copyWith(
               color:AllColors.buttonMainColor
           )
