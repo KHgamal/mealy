3d5628fe320a8fe300c70998b6fc6cd5feef86ae
Dell <khokha122002kh@gmail.com>
2024-04-16 03:16:35 +0200

    handle permission cases for get current location method

--- lib/core/utils/helpers/current_location_helper/current_location_helper.dart
+++ lib/core/utils/helpers/current_location_helper/current_location_helper.dart
@@ -1,37 +1,39 @@
+import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-
-// https://medium.com/@fernnandoptr/how-to-get-users-current-location-address-in-flutter-geolocator-geocoding-be563ad6f66a#0634
-
+// we first check whether the location service is enabled or disabled;
+// if it’s disabled, we show the snackbar and return false.
+// Then, we call the checkPermission() method to check if the user already granted permission to acquire the device’s location.
+// If the permission is denied, we call the requestPermission() method to request permission to access the device’s location.
+// If the permission is permanently denied, we show the snackbar and return false.
+// Otherwise, we return true, meaning that permissions are granted.
 class LocationHelper {
-
-  static Future<Position> getCurrentLocation() async {
-    bool isServiceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!isServiceEnabled) {
-      await Geolocator.requestPermission();
+  static Future<bool> handleLocationPermission(context) async {
+    LocationPermission permission;
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
     }
-
-    return await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
   }
+
 }
-// LocationPermission permission;
-// if (!serviceEnabled) {
-// ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-// content: Text('Location services are disabled. Please enable the services')));
-// return false;
-// }
-// permission = await Geolocator.checkPermission();
-// if (permission == LocationPermission.denied) {
-// permission = await Geolocator.requestPermission();
-// if (permission == LocationPermission.denied) {
-// ScaffoldMessenger.of(context).showSnackBar(
-// const SnackBar(content: Text('Location permissions are denied')));
-// return false;
-// }
-// }
-// if (permission == LocationPermission.deniedForever) {
-// ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-// content: Text('Location permissions are permanently denied, we cannot request permissions.')));
-// return false;
-// }
+
+
