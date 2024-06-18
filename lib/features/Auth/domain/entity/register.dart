f8227fddb49ff94f34d1adb877e856f3b6319a69
Rahma Gamal <rahmagm002@gmail.com>
2024-06-18 03:08:11 +0300

    register api

---
+++ lib/features/Auth/domain/entity/register.dart
@@ -0,0 +1,15 @@
+class RegistryAccount {
+  final String displayName;
+  final String email;
+  final String phoneNumber;
+  final String password;
+  final String confirmPassword;
+
+  RegistryAccount({
+    required this.displayName,
+    required this.email,
+    required this.phoneNumber,
+    required this.password,
+    required this.confirmPassword,
+  });
+}
